4fbad4b47681cb3a8b8cc48e9a0877ce15630e54
ERUIHNIYHBKBNF <839203521@qq.com>
2022-06-30 00:35:35 +0800

    feat: layout

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { useNavigate, Outlet } from 'react-router';
+import { Tabs } from 'antd-mobile';
+const Tab = Tabs.Tab;
 
 export default React.memo(() => {
+  const navigator = useNavigate();
   return <>
-    <div>
-      111
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
       <Outlet />
+      <Tabs onChange={(key) => navigator(key)}>
+        <Tab title="活动" key="activity" />
+        <Tab title="房源" key="list" />
+      </Tabs>
     </div>
   </>;
 });
